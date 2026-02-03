2e9773ccb59caf66c1422319196993524f9ee2c1
ChinazaBelolisa <chinazabelolisa@gmail.com>
2026-02-02 21:46:45 -0500

    project structure and datasets

---
+++ scripts/setup/init_database.sql
@@ -0,0 +1,12 @@
+/*
+=============================================================
+Create Database
+=============================================================
+WARNING:
+- Drops and recreates the database (all data deleted).
+*/
+
+DROP DATABASE IF EXISTS datawarehouse;
+CREATE DATABASE datawarehouse;
+
+USE datawarehouse;
